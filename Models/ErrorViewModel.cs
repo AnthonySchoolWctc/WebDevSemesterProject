5fa9086ec2e4ac1fefeef3dfb93a46d3ff0a83b8
AnthonySchoolWctc <Adzikowski@my.wctc.edu>
2026-09-22 18:34:42 -0500

    First creation of project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SemesterProjectVideoGames.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
